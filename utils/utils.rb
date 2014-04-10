b6952eade48fe7e6a71c02d8ed084a4aa713fb20
dcasaftei <DanConstantin.Asaftei>
2014-04-10 09:37:15 +0100

    add utils module

---
+++ utils/utils.rb
@@ -0,0 +1,21 @@
+module Utils
+
+  def bytes_nb x
+    x.to_i.size
+  end
+
+  def bits_nb x
+    8 * bytes_nb(x)
+  end
+
+  def max_integer
+    bytes_nb = 1.size
+    bits_nb = bytes_nb * 8
+    2**(bits_nb-2) -1
+  end
+
+  def min_integer
+    -max_integer - 1
+  end
+
+end
